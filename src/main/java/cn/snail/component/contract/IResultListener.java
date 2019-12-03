0a601665804efe36b74b289d91e5718075f72d5c
yanggang1 <yanggang1@coocaa.com>
2019-12-03 10:50:13 +0800

    feat: 添加receiver的判断以及结果的回调监听

---
+++ src/main/java/cn/snail/component/contract/IResultListener.java
@@ -0,0 +1,11 @@
+package cn.snail.component.contract;
+
+/**
+ * @Autohor Yang Gang
+ * @Date 2019/12/3
+ * @Desc applied to ...
+ */
+public interface IResultListener<T> {
+
+    void onResult(T t);
+}
--- src/main/java/cn/snail/component/receiver/BaseReceiver.java
+++ src/main/java/cn/snail/component/receiver/BaseReceiver.java
@@ -4,8 +4,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.AndroidRuntimeException;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import cn.snail.component.contract.IResultListener;
 
 /**
  * @Autohor Yang Gang
@@ -15,38 +19,52 @@ import androidx.annotation.NonNull;
 public class BaseReceiver extends BroadcastReceiver {
 
     private Context mContext;
+    private IResultListener<Intent> mListener;
 
     public BaseReceiver(@NonNull Context context) {
         mContext = context;
+    }
+
+    public BaseReceiver(@NonNull Context context, @Nullable IResultListener<Intent> listener) {
+        mContext = context;
+        mListener = listener;
     }
 
     public Intent register() {
         return register(getIntentFilter());
     }
 
-    public Intent register(IntentFilter filter) {
+    public Intent register(@NonNull IntentFilter filter) {
         return mContext.registerReceiver(this, filter);
     }
 
     public void unRegister() {
         mContext.unregisterReceiver(this);
+        mListener = null;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        if (mListener != null) {
+            mListener.onResult(intent);
+        }
     }
 
     /**
      * 需要注册的action集合
      */
+    @NonNull
     protected String[] getActions() {
         return new String[0];
     }
 
     private IntentFilter getIntentFilter() {
         IntentFilter intentFilter = new IntentFilter();
-        for (String it : getActions()) {
+        String[] actions = getActions();
+        if (actions.length == 0) {
+            throw new AndroidRuntimeException("actions should not be ignored!");
+        }
+        for (String it : actions) {
             intentFilter.addAction(it);
         }
         return intentFilter;
